9d90dde0227818dcb5fdf99129a6a3ef78b16de4
aston <356386@qq.com>
2021-02-25 02:14:10 +0800

    fix cache

--- src/main/java/cn/foperate/ros/verticle/DnsVeticle.kt
+++ src/main/java/cn/foperate/ros/verticle/DnsVeticle.kt
@@ -28,7 +28,11 @@ class DnsVeticle: AbstractVerticle() {
     private lateinit var serverSocket:DatagramSocket  // 正在监听的服务端口
     private lateinit var eb: EventBus
 
-    private val resultCache = CacheBuilder.newBuilder()
+    private val aCache = CacheBuilder.newBuilder()
+        .expireAfterWrite(1, TimeUnit.HOURS)
+        .maximumSize(1000)
+        .build<String, Buffer>()
+    private val httpsCache = CacheBuilder.newBuilder()
         .expireAfterWrite(1, TimeUnit.HOURS)
         .maximumSize(1000)
         .build<String, Buffer>()
@@ -50,30 +54,55 @@ class DnsVeticle: AbstractVerticle() {
                     val message = Message(request.data().bytes)
                     val questionName = message.question.name.toString()
                     val questionType = message.question.type
-                    log.debug(questionName)
-                    resultCache.getIfPresent(questionName)?.let {
-                        log.debug("Found matched answer from cache")
-                        val answer = Message(it.bytes)
-                        answer.header.id = message.header.id
-                        val buffer = Buffer.buffer(answer.toWire())
-                        ss.send(buffer, request.sender().port(), request.sender().host())
-                            .subscribe().with {}
-                        return@reply
-                    }
-                    if (questionType==Type.A) when {
-                        DomainUtil.match(questionName) -> {
-                            log.debug("gfwlist hint")
-                            forwardToRemote(request, questionName)
+                    val typeString = Type.string(questionType)
+                    log.debug("$questionName : $typeString")
+
+                    when (questionType) {
+                        Type.A -> {
+                            aCache.getIfPresent(questionName)?.let {
+                                log.debug("Found matched answer from cache")
+                                val answer = Message(it.bytes)
+                                answer.header.id = message.header.id
+                                val buffer = Buffer.buffer(answer.toWire())
+                                ss.send(buffer, request.sender().port(), request.sender().host())
+                                    .subscribe().with {}
+                                return@reply
+                            }
+                            when {
+                                DomainUtil.match(questionName) -> {
+                                    log.debug("gfwlist hint")
+                                    forwardToRemote(request, questionName, questionType)
+                                }
+                                DomainUtil.matchBlock(questionName) -> {
+                                    log.debug("adBlock matched")
+                                    val reply = blockMessage(message)
+                                    ss.send(Buffer.buffer(reply), request.sender().port(), request.sender().host())
+                                        .subscribe().with {}
+                                }
+                                else -> forwardToFallback(request, questionName, questionType)
+                            }
                         }
-                        DomainUtil.matchBlock(questionName) -> {
-                            log.debug("adBlock matched")
-                            val reply = blockMessage(message)
-                            ss.send(Buffer.buffer(reply), request.sender().port(), request.sender().host())
-                                .subscribe().with {}
+                        Type.HTTPS -> {
+                            // 不确定HTTPS请求是否会有回应。
+                            httpsCache.getIfPresent(questionName)?.let {
+                                log.debug("Found matched answer from cache")
+                                val answer = Message(it.bytes)
+                                answer.header.id = message.header.id
+                                val buffer = Buffer.buffer(answer.toWire())
+                                ss.send(buffer, request.sender().port(), request.sender().host())
+                                    .subscribe().with {}
+                                return@reply
+                            }
+                            if (DomainUtil.match(questionName)) {
+                                log.debug("gfwlist hint")
+                                forwardToRemote(request, questionName, questionType)
+                            } else {
+                                forwardToFallback(request, questionName, questionType)
+                            }
                         }
-                        else -> forwardToFallback(request, questionName)
-                    } else {
-                        forwardToFallback(request, questionName)
+                        else ->  {
+                            forwardToFallback(request, questionName, questionType)
+                        }
                     }
                 }
             }
@@ -86,32 +115,43 @@ class DnsVeticle: AbstractVerticle() {
         return Uni.createFrom().voidItem()
     }
 
-    private fun forwardToRemote(packet: DatagramPacket, name: String) {
+    private fun cacheResult(name:String, type:Int, buffer: Buffer) {
+        if (type==Type.A) {
+            aCache.put(name, buffer)
+        } else if (type==Type.HTTPS) {
+            httpsCache.put(name, buffer)
+        }
+    }
+
+    private fun forwardToRemote(packet: DatagramPacket, name: String, type: Int) {
         val startTime = System.currentTimeMillis()
         val clientSocket = vertx.createDatagramSocket()
         clientSocket.send(packet.data(), remotePort, remote)
             .subscribe().with ({
                 clientSocket.toMulti().subscribe().with { result ->
-                    processResult(packet, result, startTime)
+                    if (type==Type.A) {
+                        processResult(packet, result, startTime)
+                    }
                     clientSocket.closeAndForget()
-                    resultCache.put(name, result.data())
+                    cacheResult(name, type, result.data())
                 }
             }) {
-                forwardToFallback(packet, name)
+                forwardToFallback(packet, name, type)
                 clientSocket.closeAndForget()
             }
     }
 
-    private fun forwardToFallback(request: DatagramPacket, name: String) {
+    private fun forwardToFallback(request: DatagramPacket, name: String, type: Int) {
         val clientSocket = vertx.createDatagramSocket()
         clientSocket.send(request.data(), 53, fallback)
             .subscribe().with ({
                 clientSocket.toMulti().subscribe().with { response ->
+                    log.debug("Get answers for ${Type.string(type)}")
                     // Fallback服务当作不可信信息，不操作IPset列表
                     serverSocket.send(response.data(), request.sender().port(), request.sender().host())
-                        .subscribe().with { }
+                        .subscribe().with {}
                     clientSocket.closeAndForget()
-                    resultCache.put(name, response.data())
+                    cacheResult(name, type, response.data())
                 }
             }) {
                 clientSocket.closeAndForget()
