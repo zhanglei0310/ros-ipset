341270b56dc2a795b2b625208f12ebbfc9d893bb
aston <356386@qq.com>
2022-04-15 14:13:13 +0800

    fix: Doh中的WebClient使用不稳定，尝试修改实现。

--- src/main/kotlin/cn/foperate/ros/service/RestService.kt
+++ src/main/kotlin/cn/foperate/ros/service/RestService.kt
@@ -21,16 +21,16 @@ object RestService {
 
     fun init(vertx: Vertx, config: JsonObject) {
         client = WebClient.create(vertx, webClientOptionsOf(
+            connectTimeout = 5000,
             defaultHost = config.getString("host"),
             defaultPort = config.getInteger("port", 443),
-            protocolVersion = HttpVersion.HTTP_1_1, // 应该没有KEEP_ALIVE功能
             keepAlive = true,
             keepAliveTimeout = 60,
+            maxPoolSize = 2,
+            protocolVersion = HttpVersion.HTTP_1_1, // 应该没有KEEP_ALIVE功能
             ssl = true,
             trustAll = true,
             verifyHost = false,
-            connectTimeout = 5000,
-            poolEventLoopSize = 1
         ))
         rosListKey = config.getString("listName")
         rosUser = config.getString("user")
